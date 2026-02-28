0a95008a6435a6cf07b7bb44cc8a427482075172
Alessito54 <arielpedraza10@outlook.com>
2026-02-27 22:43:52 -0600

    feat: update promo video URL to a YouTube embed link in config.js

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const SOCIAL_FACEBOOK = "https://facebook.com";
 export const SOCIAL_INSTAGRAM = "https://instagram.com";
 export const SOCIAL_TIKTOK = "https://tiktok.com";
 export const CONTACT_EMAIL = "contacto@oasys.com";
-export const PROMO_VIDEO_URL = "/src/assets/videos/Adiós a la sequía_ el invernadero inteli 2026-02-28.mp4"; // E.g., "https://www.youtube.com/embed/YOUR_VIDEO_ID"
+export const PROMO_VIDEO_URL = "https://www.youtube.com/embed/DsDrLTyDsAA"; // E.g., "https://www.youtube.com/embed/YOUR_VIDEO_ID"
